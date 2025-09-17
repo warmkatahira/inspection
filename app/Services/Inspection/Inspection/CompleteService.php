5193c5f891095d00644722b1a52fe2e2154397d5
warmkatahira <t.katahira@warm.co.jp>
2025-09-17 10:58:06 +0900

    20250917_1

--- app/Services/Inspection/Inspection/CompleteService.php
+++ app/Services/Inspection/Inspection/CompleteService.php
@@ -16,11 +16,23 @@ class CompleteService
         if(is_null(session('item_jan_code'))){
             throw new \RuntimeException('検品中ではありません。');
         }
-        // 検品情報を更新
-        Item::where('item_jan_code', session('item_jan_code'))->update([
-            'inspection_quantity' => session('inspection_quantity'),
-            'is_completed' => 1,
-            'inspected_at' => CarbonImmutable::now(),
-        ]);
+        // 商品を取得
+        $item = Item::where('item_jan_code', session('item_jan_code'))->first();
+        // 既に検品が実施済みの場合
+        if($item->is_completed){
+            // 検品情報を更新(検品数量を加算)
+            $item->increment('inspection_quantity', session('inspection_quantity'), [
+                'inspected_at' => CarbonImmutable::now(),
+            ]);
+        }
+        // 初めての検品の場合
+        if(!$item->is_completed){
+            // 検品情報を更新
+            $item->update([
+                'inspection_quantity' => session('inspection_quantity'),
+                'is_completed' => 1,
+                'inspected_at' => CarbonImmutable::now(),
+            ]);
+        }
     }
 }
